6994b8859bed7926030b341559075462c0fb9970
asadbek2304 <abduxowimoff@gmail.com>
2021-04-27 14:22:24 +0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Checkout from "./CheckoutPage";
+import Private from "./PrivateRoute";
+
+export { Home, About, Cart, Error, Products, SingleProduct, Checkout, Private };
